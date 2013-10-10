c5a6066317f0045924ce8ae2944d4d0e12eb141b
oterox <oterox@gmail.com>
2013-10-10 19:32:55 +0200

    added new entities

---
+++ src/Pixellary/InhouseBundle/Entity/TaskStatus.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Pixellary\InhouseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TaskStatus
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class TaskStatus
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=50)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return TaskStatus
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
