7c6456f31a7210d76af441ea195a48d4f0625443
oterox <oterox@gmail.com>
2013-11-01 13:46:46 +0100

    latest changes

--- src/Pixellary/InhouseBundle/Entity/TaskStatus.php
+++ src/Pixellary/InhouseBundle/Entity/TaskStatus.php
@@ -6,25 +6,16 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * TaskStatus
- *
- * @ORM\Table()
- * @ORM\Entity
  */
 class TaskStatus
 {
     /**
      * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
      * @var string
-     *
-     * @ORM\Column(name="title", type="string", length=50)
      */
     private $title;
 
